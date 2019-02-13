a9d7b795296dc955a0c0fbae78d92bd68be5e7c8
OuyangWei <18344175265@126.com>
2019-02-13 09:17:28 +0800

    2019-2-13 9:17 加入了增删改查的异常处理，完善demo

--- src/main/java/com/wz/controllers/SpringmvcController.java
+++ src/main/java/com/wz/controllers/SpringmvcController.java
@@ -3,6 +3,7 @@ package com.wz.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wz.entities.User;
@@ -11,19 +12,19 @@ import com.wz.services.UserService;
 
 @Controller
 public class SpringmvcController {
-    
+
     @Autowired
     UserService userServiceImpl;
-    
+
     @RequestMapping(value="/springmvc")
     public String springmvc() {
 	return "springmvc";
     }
-    
+
     @ResponseBody
     @RequestMapping(value="/insert")
     public String insert() {
-	
+
 	User user = new User();
 	user.setId(4);
 	user.setUsername("小东");
@@ -31,60 +32,68 @@ public class SpringmvcController {
 	user.setIdentityCard("12345");
 	user.setPhoneNumber("54321");
 	user.setJob("研发部经理");
-	
-	userServiceImpl.insert(user);
-	
-	return "insert success";
+
+	try {
+	    userServiceImpl.insert(user);
+	    return "insert success";	    
+	} catch (Exception e) {
+	    return "insert failed,may be user that id=4 existed.";
+	}
     }
-    
+
     @ResponseBody
     @RequestMapping(value="/update")
     public String update() {
-	
-	User user = userServiceImpl.selectUserById(4);
-	user.setUsername("大东");
-	user.setJob("小兵");
-	
-	userServiceImpl.update(user);
-	
-	return "update success";
+
+	try {
+	    User user = userServiceImpl.selectUserById(4);
+	    user.setUsername("大东");
+	    user.setJob("小兵");
+	    userServiceImpl.update(user);
+	    return "update success";	    
+	} catch (Exception e) {
+	    return "update failed,user that id=4 is not exist.";
+	}
     }
-    
+
     @ResponseBody
-    @RequestMapping(value="/select",produces="text/plain;charset=UTF-8")
+    @RequestMapping(value="/select",produces="text/plain;charset=UTF-8",method=RequestMethod.GET)
     public String select() {
-	
-	User user = userServiceImpl.selectUserById(4);
-	String tmp = "select success! \n";
-	tmp += "User: \n";
-	tmp += "id: ";
-	tmp += user.getId();
-	tmp += "\n";
-	tmp += "username: ";
-	tmp += user.getUsername();
-	tmp += "\n";
-	tmp += "password: ";
-	tmp += user.getPassword();
-	tmp += "\n";
-	tmp += "identity_card: ";
-	tmp += user.getIdentityCard();
-	tmp += "\n";
-	tmp += "phone_number: ";
-	tmp += user.getPhoneNumber();
-	tmp += "\n";
-	tmp += "job: ";
-	tmp += user.getJob();
-	tmp += "\n";
-	
-	return tmp;
+
+	try {
+	    User user = userServiceImpl.selectUserById(4);
+	    String tmp = "select success! \n";
+	    tmp += "User: \n";
+	    tmp += "id: ";
+	    tmp += user.getId();
+	    tmp += "\n";
+	    tmp += "username: ";
+	    tmp += user.getUsername();
+	    tmp += "\n";
+	    tmp += "password: ";
+	    tmp += user.getPassword();
+	    tmp += "\n";
+	    tmp += "identity_card: ";
+	    tmp += user.getIdentityCard();
+	    tmp += "\n";
+	    tmp += "phone_number: ";
+	    tmp += user.getPhoneNumber();
+	    tmp += "\n";
+	    tmp += "job: ";
+	    tmp += user.getJob();
+	    tmp += "\n";
+
+	    return tmp; 
+	} catch (Exception e) {
+	    return "Seleted failed,user that id=4 is not exist.";
+	}
     }
-    
+
     @ResponseBody
     @RequestMapping(value="/delete")
     public String delete() {
-	
-	userServiceImpl.delete(4);
-	
+
+	userServiceImpl.delete(4);	    
 	return "delete success";
     }
 }
